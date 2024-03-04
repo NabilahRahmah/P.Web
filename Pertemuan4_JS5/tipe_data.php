0d0158196098d88ec1bb5e8a8a9ecd22786b6714
NabilahRahmah <nabilah.oppo13@gmail.com>
2024-03-04 12:55:30 +0700

    p4_variabel_konstanta_perubahan

---
+++ Pertemuan4_JS5/tipe_data.php
@@ -0,0 +1,56 @@
+<?php
+//data pertama pada file tipe data
+$a = 10;
+$b = 5;
+$c = $a + 5;
+$d = $b + (10 * 5);
+$e = $d - $c;
+
+echo "Variabel a: {$a} <br>";
+echo "Variabel b: {$b} <br>";
+echo "Variabel c: {$c} <br>";
+echo "Variabel d: {$d} <br>";
+echo "Variabel e: {$e} <br>";
+
+var_dump($e);
+echo "<br><br>";
+
+//tambahan isi file tipe data perhitungan rata2
+$nilaiMatematika = 5.1;
+$nilaiIPA = 6.7;
+$nilaiBahasaIndonesia = 9.3;
+
+$rataRata = ($nilaiMatematika + $nilaiIPA + $nilaiBahasaIndonesia) / 3;
+
+echo "Matematika: {$nilaiMatematika} <br>";
+echo "IPA: {$nilaiIPA} <br>";
+echo "Bahasa Indonesia: {$nilaiBahasaIndonesia} <br>";
+echo "Rata-rata: {$rataRata} <br>";
+
+var_dump($rataRata);
+
+//tambah file true or false
+echo "<br><br>";
+$apakahSiswaLulus = true;
+$apakahSiswaSudahujian = false;
+
+var_dump($apakahSiswaSudahujian);
+echo "<br>";
+var_dump($apakahSiswaSudahujian);
+
+//tambah file tipe data nama depan dan nama belakang
+echo "<br><br>";
+$namaDepan = "Ibnu";
+$namaBelakang = 'Jakaria';
+
+$namaLengkap = "{$namaDepan} {$namaBelakang}";
+$namaLengkap2 = $namaDepan . '' . $namaBelakang;
+
+echo "Nama Depan: {$namaDepan} <br>";
+echo $namaLengkap;
+
+//tambah data list mahasiswa
+echo "<br><br>";
+$listMahasiswa = ["Wahid Abdullah", "Elmo Bachtiar", "Lendis Fabri"];
+echo $listMahasiswa[0];
+?>
--- Pertemuan4_JS5/variabel_konstanta.php
+++ Pertemuan4_JS5/variabel_konstanta.php
@@ -1,7 +1,18 @@
 <?php
-
+// kode konstanta pertama
 $angka1 = 10;
-$angka2 = 4;
-$hasil = $angka1 +$angka2;
+$angka2 = 5;
+$hasil = $angka1 + $angka2;
 echo "Hasil penjumlahan $angka1 dan $angka2 adalah $hasil.";
+echo "<br>";
+//penambahan isi file selanjutnya
+$benar = true;
+$salah = false;
+echo "Variabel benar: $ benar, Variabel salah: $salah";
+
+//Mendefinisikan konstanta untuk nilai tetap
+define("NAMA_SITUS", "WebsiteKu.com");
+define("TAHUN_PENDIRIAN", 2023);
+echo "<br>";
+echo "Selamat datang di ". NAMA_SITUS . ", situs yang didirikan pada tahun " . TAHUN_PENDIRIAN . ".";
 ?>
