0844813d86de7ae76843fe4219c213615f4c5866
NabilahRahmah <nabilah.oppo13@gmail.com>
2024-04-11 20:10:22 +0700

    Melanjutkan js 11 dan js 12

---
+++ Pertemuan12/oop/ConstructorDestructor.php
@@ -0,0 +1,33 @@
+<?php
+// Basic OOP Langkah 10  
+// CONSTRUSTOR DAN DESTRUCTORS
+class Car
+{
+    private $brand; // Properti untuk merepresentasikan merek mobil
+
+    // Konstruktor untuk menciptakan objek mobil dengan merek tertentu
+    public function __construct($brand)
+    {
+        echo "A new car is created. <br>"; // Menampilkan pesan bahwa mobil baru telah diciptakan
+        $this->brand = $brand; // Menginisialisasi properti brand dengan nilai yang diberikan
+    }
+
+    // Metode untuk mendapatkan merek mobil
+    public function getBrand()
+    {
+        return $this->brand; // Mengembalikan merek mobil
+    }
+
+    // Destruktor untuk menampilkan pesan ketika objek mobil dihancurkan
+    public function __destruct()
+    {
+        echo "The car is destroyed. <br>"; // Menampilkan pesan bahwa mobil telah dihancurkan
+    }
+}
+
+// Membuat objek Car dengan merek "Toyota"
+$car = new Car("Toyota");
+
+// Menampilkan merek mobil
+echo "Brand: " . $car->getBrand() . "<br>"; // Menampilkan merek mobil yang telah diinisialisasi
+?>
